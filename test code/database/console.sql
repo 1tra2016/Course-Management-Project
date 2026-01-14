4a1d60f64b4fb53974a3d60e6e5e053f26375922
Chè <119411856+1tra2016@users.noreply.github.com>
2026-01-14 19:18:33 +0700

    Delete test code/database directory

--- test code/database/console.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE books (
-                       id SERIAL PRIMARY KEY,
-                       title VARCHAR(255) NOT NULL,
-                       author VARCHAR(255) NOT NULL,
-                       published_year INT NOT NULL,
-                       price DECIMAL(10,2) NOT NULL,
-                       CONSTRAINT unique_book UNIQUE (title, author)
-);
